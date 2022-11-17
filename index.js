e1d918cf5697e46d69d52b8402f868d7659a6fe3
Fahad123k <ahmad.nishu08@gmail.com>
2022-11-17 20:48:07 +0530

    router created

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app http://localhost:${port}`))
+// app.get('/', (req, res) => res.send('Hello World!'))
+// use as express router
+app.use('/',require('./routes'))
+
+app.listen(port, function(err){ 
+    if(err){
+        
+        console.log(`some error occured like ${err}`)
+    }
+    console.log(`our  app running on http://localhost:${port}`)
+
+})
