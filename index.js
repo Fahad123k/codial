282bb2a395836bb7ba31c61e318fd62b4a9c8a3f
Fahad123k <ahmad.nishu08@gmail.com>
2022-11-18 15:33:01 +0530

    format index.js

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const app = express()
 const port = 8000
 const path=require('path')
 
-app.set('view engine','ejs')
-// app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/',require('./routes'))
 
 // let choose our view or template directory as public
+app.set('view engine','ejs')
 // create a new folder for view
 app.set('views',path.join(__dirname,'views'))
 
-app.use('/',require('./routes'))
 
 app.listen(port, function(err){ 
     if(err){
