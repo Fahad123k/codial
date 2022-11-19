05e088e0a4b654d68aba8ea16cc35bdbbb1468ed
Fahad123k <ahmad.nishu08@gmail.com>
2022-11-19 11:55:39 +0530

    linking connection to mongoDB

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db= mongoose.connection;
+
+db.on('error',console.error.bind(console,"Error to connecting to mongoDB"));
+
+db.once('open',function(){
+    console.log("conneected to database")
+})
+
+module.exports=db;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000
 const path=require('path')
 const expressLayouts= require('express-ejs-layouts');
-
+const db= require('./config/mongoose')
 app.use(express.static('./assets'))
 app.use(expressLayouts)
 app.set('layout extractStyles',true)
