7c06d0d0547d37e22605250d7d5c8e0408828086
Fahad123k <ahmad.nishu08@gmail.com>
2022-12-12 20:37:52 +0530

    add passport auth to posts

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,11 @@
 const express=require('express');
 const router=express.Router();
 
+const passport= require('passport')
 const postController=require('../controller/posts_controller');
 
 
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 // router.get('/post',postController.post);
 
 module.exports=router;
